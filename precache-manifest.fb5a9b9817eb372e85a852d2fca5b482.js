5a724c003a9b287d7074af1588251b873228ebd9
christoforoskon <xristoforos.knks@yahoo.gr>
2020-10-06 16:25:25 +0300

    Updates

---
+++ precache-manifest.fb5a9b9817eb372e85a852d2fca5b482.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4827da5df71aecb11b94819ebc78dc",
+    "url": "/DogBreedZ/index.html"
+  },
+  {
+    "revision": "a45304df7ded3d5fb938",
+    "url": "/DogBreedZ/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8e8a1af94999c4d5a1bc",
+    "url": "/DogBreedZ/static/css/main.336d90dc.chunk.css"
+  },
+  {
+    "revision": "a45304df7ded3d5fb938",
+    "url": "/DogBreedZ/static/js/2.b2df12b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DogBreedZ/static/js/2.b2df12b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e8a1af94999c4d5a1bc",
+    "url": "/DogBreedZ/static/js/main.06d88d07.chunk.js"
+  },
+  {
+    "revision": "982df4ca0c1134235eca",
+    "url": "/DogBreedZ/static/js/runtime-main.de5bef96.js"
+  },
+  {
+    "revision": "cbf65b6fdcafd3c17bfaa8fbb6865ff4",
+    "url": "/DogBreedZ/static/media/CARTOON FREE.cbf65b6f.woff"
+  }
+]);
